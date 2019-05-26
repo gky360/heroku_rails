6d64fa97a7c8428045bca7b963f5bf61645a8760
gky360 <gky360@gmail.com>
2019-05-26 14:35:34 +0900

    fix logs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,18 @@ class WelcomeController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    logger.debug "\n\ncookies --------------------"
     cookies.each do |key, val|
+      logger.debug "#{key}:\n#{val}"
+    end
+
+    logger.debug "\n\nheaders --------------------"
+    request.headers.each do |key, val|
+      logger.debug "#{key}:\n#{val}"
+    end
+
+    logger.debug "\n\nparams --------------------"
+    params.each do |key, val|
       logger.debug "#{key}:\n#{val}"
     end
   end
